61db7a58607a930e5fd667d14f4945793f892f41
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-15 20:16:49 +0200

    courbet: overlay-lineage: Specify device has a notch
    Change-Id: Ib912728020f85c820b10d5102365dc37f9a54342

--- device.mk
+++ device.mk
@@ -72,7 +72,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
