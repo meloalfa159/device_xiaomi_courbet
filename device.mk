15e30c4d1ba8cce1c245c57e12375387e88b1f2f
Arian <arian.kulmer@web.de>
2021-06-18 18:41:53 +0200

    courbet: power: Import powerhint configuration from Pixel 4A
    powerhint.json is checked out to https://android.googlesource.com/device/google/sunfish/+/refs/tags/android-11.0.0_r38/powerhint.json.
    
    Change-Id: Ib576cea82363865cefd5e5fa2e7c746c0b95c3a8

--- device.mk
+++ device.mk
@@ -311,6 +311,9 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
