4cdeb0dee4c55eab640dbcedf386e428cc9c7f8c
Noonlord <mymmym22@gmail.com>
2021-09-17 01:15:57 +0300

    courbet: Define kernel config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,8 @@
 
 DEVICE_PATH := device/xiaomi/courbet
 
+# Kernel
+TARGET_KERNEL_CONFIG := courbet_user_defconfig
+
 # Inherit from proprietary files
 include vendor/xiaomi/courbet/BoardConfigVendor.mk
