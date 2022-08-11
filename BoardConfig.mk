f341c6d09006a4c6d62e5854251f92e0c0b5aea4
TheScarastic <warabhishek@gmail.com>
2022-08-11 13:47:36 +0530

    courbet: sepolicy: add XiaomiParts to its own domain
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -218,6 +218,8 @@ TARGET_SCREEN_DENSITY := 440
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
