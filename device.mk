2f9e4fb2376b51ed53b674c876d8f87a618a7a07
youngherna <hernan.mauricio16@gmail.com>
2022-09-25 20:04:16 +0200

    Revert "sm6150-common: Move to common fingerprint HIDL"
    This reverts commit cc3ba5c9c22a32682ef37c3f3afe5dea46ccd43a.

--- device.mk
+++ device.mk
@@ -138,9 +138,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
 
 # Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
