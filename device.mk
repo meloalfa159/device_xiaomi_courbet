320feefe2d516710a76871ec3bbc4a2eec71508e
youngherna <hernan.mauricio16@gmail.com>
2022-10-01 23:14:49 +0200

    courbet: build fingerprint hal

--- device.mk
+++ device.mk
@@ -157,6 +157,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.courbet
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
