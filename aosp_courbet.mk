1ec565562c01937e3b49fd2590340c4ba31b4bd4
basamaryan <basam.aryan@gmail.com>
2023-01-22 06:32:11 +0000

    courbet: Welcome PixelOS
    Change-Id: I6bdbd62a1deb9b0c32e46f2c91d483988bb3481c

--- aosp_courbet.mk
+++ aosp_courbet.mk
@@ -11,10 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from courbet device
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_courbet
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := aosp_courbet
 PRODUCT_DEVICE := courbet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 4G
