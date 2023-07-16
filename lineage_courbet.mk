2fc44c6ef1f3c95d6b2b524e7114226aeb91aec1
basamaryan <basam.aryan@gmail.com>
2023-07-16 14:26:57 +0530

    courbet: Update blobs from MIUI V14.0.3.0.TKQMIXM
    Change-Id: I3f580db028b1f330961e3ca0a3320ef86047d69d

--- lineage_courbet.mk
+++ lineage_courbet.mk
@@ -27,6 +27,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="courbet_global-user 13 RKQ1.210614.002 V14.0.2.0.TKQMIXM release-keys"
+    PRIVATE_BUILD_DESC="courbet_global-user 13 TKQ1.221013.002 V14.0.3.0.TKQMIXM release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/courbet_global/courbet:13/RKQ1.210614.002/V14.0.2.0.TKQMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/courbet_global/courbet:13/TKQ1.221013.002/V14.0.3.0.TKQMIXM:user/release-keys
