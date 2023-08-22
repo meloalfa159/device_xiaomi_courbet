baee23260aff6180a09ab720870f9b1a0635f54d
youngherna <hernan.mauricio16@gmail.com>
2023-08-22 23:52:38 +0000

    courbet: hidl: add android.hardware.media.c2@1.0 to tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,7 +76,6 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_courbet
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
-DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/device_framework_compatibility_matrix.xml \
                                               $(DEVICE_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml \
