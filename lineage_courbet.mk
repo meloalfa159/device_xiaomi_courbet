e16f5d4dd363ad7d9bd84a889e428b8fa6d91da2
meloalfa159 <melolongo87@gmail.com>
2023-09-09 21:34:11 +0200

    LineageOS

--- lineage_courbet.mk
+++ lineage_courbet.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from courbet device
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
-# Inherit some common Ancient OS stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Boot animation
