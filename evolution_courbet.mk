a98b310e1d97d95c9c472017708f167d9e52bda0
meloalfa159 <melolongo87@gmail.com>
2023-09-09 13:39:32 +0200

    EvolutionX

--- evolution_courbet.mk
+++ evolution_courbet.mk
@@ -11,14 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from courbet device
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
-# Inherit some common Ancient OS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common EvolutionX stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_NAME := lineage_courbet
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USE_PIXEL_FINGERPRINT := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
+TARGET_USES_PICO_GAPPS := true
+
+PRODUCT_NAME := evolution_courbet
 PRODUCT_DEVICE := courbet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2101K9AG
