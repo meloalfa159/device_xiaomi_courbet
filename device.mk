7d1070d3b4cfca04a19e9275369d48c6c08b2ede
meloalfa159 <melolongo87@gmail.com>
2024-02-08 19:30:14 +0100

    courbet: use sweet miuicamera

--- device.mk
+++ device.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 
 # Call the MiuiCamera setup
-$(call inherit-product-if-exists, vendor/xiaomi/courbet-miuicamera/products/miuicamera.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/sweet-miuicamera/products/miuicamera.mk)
 
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
