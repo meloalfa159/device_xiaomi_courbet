9341c84b1b8d4236f0605563df6f1674fa5bc724
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-05-06 15:24:50 +0545

    courbet: switch to common AIDL IR service
    Change-Id: I36c05aaa312969e479896c6e63a26a68cf897afa
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -59,7 +59,7 @@ PRODUCT_COPY_FILES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir-service.xiaomi
+    android.hardware.ir-service.lineage
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
