1d06c651957de0c3c6c096a97ce0f96e13789592
meloalfa159 <melolongo87@gmail.com>
2025-07-21 18:36:03 +0200

    courbet: InfinityX bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_courbet.mk
+    $(LOCAL_DIR)/infinity_courbet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_courbet-user \
-    lineage_courbet-userdebug \
-    lineage_courbet-eng
+    infinity_courbet-user \
+    infinity_courbet-userdebug \
+    infinity_courbet-eng
--- infinity_courbet.mk
+++ infinity_courbet.mk
@@ -13,13 +13,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_courbet
+PRODUCT_NAME := infinity_courbet
 PRODUCT_DEVICE := courbet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 4G
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Infinity specific
+TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GAPPS := true
+INFINITY_MAINTAINER := melo159
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
